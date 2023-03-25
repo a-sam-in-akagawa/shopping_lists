d31ab9b102e613319319980540a761f0dbc59ea4
asami.nakagawa <nakagawa.asami@coresite.ne.jp>
2023-03-25 04:22:07 +0000

    login pages

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,8 @@
     <div class="prose hero bg-base-200 mx-auto max-w-full rounded">
         <div class="hero-content text-center my-10">
             <div class="max-w-md mb-10">
-                <h2>思いついたらすぐにメモ！<br>シンプルなお買い物リストを作りましょう！</h2>
+                <h4>思いついたらすぐにメモ！<br>シンプルなお買い物リストを作りましょう！</h4>
+                <br>
                 {{-- ユーザ登録ページへのリンク --}}
                 <a class="btn btn-neutral btn-lg normal-case" href="{{ route('register') }}">新規登録</a>
             </div>
